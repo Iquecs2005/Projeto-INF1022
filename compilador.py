0b3550441814a09c9606256e3070b464fd3c5be9
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-06-17 22:28:41 -0300

    Lexer inicial

--- compilador.py
+++ compilador.py
@@ -1,3 +1,13 @@
 from lexer import ObsActLexer
-from parser import ObsActParser
+#from parser import ObsActParser
 
+lexer = ObsActLexer()
+#parser = ObsActParser()
+
+arq = open('entrada.txt', 'r')
+
+data = arq.read()
+for tok in lexer.tokenize(data):
+    print(tok)
+
+arq.close()
--- lexer.py
+++ lexer.py
@@ -1,4 +1,20 @@
 from sly import Lexer
 
 class ObsActLexer(Lexer):
+    tokens = {"dispositivo", "namedevice"}
+    literals = {':', '{', '}'}
+
+    ignore = ' \t'
+
+    dispositivo = r'dispositivo'
+    namedevice = r'[a-zA-Z]+'
+
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+        self.index += 1
+
     pass
